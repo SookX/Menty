2603020e36c88c11ab639f8b4182d94d13aece7c
MrTiNnN <martin.v.velchev@gmail.com>
2024-09-28 12:20:20 +0300

    Create client
    Made a React JS app
    Added a context provider

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import DataProvider from "./context/DataContext"
+
+function App() {
+
+  return (
+    <DataProvider>
+      App
+    </DataProvider>
+  )
+}
+
+export default App
---
+++ client/src/context/DataContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useEffect, useState } from "react";
+
+export const DataContext = createContext({})
+
+const DataProvider = ({ children }) => {
+
+    return (
+        <DataContext.Provider value={{
+
+            }}>
+
+            {children}
+
+        </DataContext.Provider>
+    )
+}
+
+export default DataProvider
