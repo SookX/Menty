27704b720833ff19ce490de6a3d4e18c3f5975c0
MrTiNnN <martin.v.velchev@gmail.com>
2024-09-28 13:53:32 +0300

    Routing
    Installed react-router-dom
    Setup the routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
 import LayoutGrid from "./components/LayoutGrid/LayoutGrid"
 import DataProvider from "./context/DataContext"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home"
 
 function App() {
 
   return (
-    <DataProvider>
-      <LayoutGrid
-        columns={12}
-        padding='6.4rem'
-        gutter='2.4rem'
-        type='screen'
-      />
-    </DataProvider>
+    <BrowserRouter>
+      <DataProvider>
+        <LayoutGrid
+          columns={12}
+          padding='6.4rem'
+          gutter='2.4rem'
+          type='screen'
+        />
+
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+
+        </Routes>
+
+      </DataProvider>
+    </BrowserRouter>
   )
 }
 
