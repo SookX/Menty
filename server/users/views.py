5be7ac5f0c54b9bdaaa4c5c57e579bef27cc587e
anastassow <127148449+anastassow@users.noreply.github.com>
2024-09-28 13:41:12 +0300

    description

--- server/users/views.py
+++ server/users/views.py
@@ -9,6 +9,17 @@ from django.shortcuts import get_object_or_404
 
 @api_view(['POST'])
 def register(request):
+    
+    """
+    Registers a new user with the provided email, username, and password.
+
+    Args:
+    - request (Request): The incoming HTTP request containing user registration data.
+
+    Returns:
+    - Response: A JSON response indicating success or failure of the registration process.
+    """
+    
     email = request.data.get('email')
     username = request.data.get('username')
     password = request.data.get('password')
@@ -27,6 +38,17 @@ def register(request):
 
 @api_view(['POST'])
 def login(request):
+
+    """
+    Authenticates a user with the provided email and password.
+
+    Args:
+    - request (Request): The incoming HTTP request containing user login credentials.
+
+    Returns:
+    - Response: A JSON response containing access and refresh tokens if authentication is successful, or an error message.
+    """
+
     email = request.data.get('email')
     password = request.data.get('password')
 
@@ -46,6 +68,18 @@ def login(request):
 @api_view(['GET', 'PATCH', 'DELETE'])
 def user_details(request, id):
 
+    """
+    Retrieves, updates, or deletes a specific user based on the provided ID.
+
+    Args:
+    - request (Request): The incoming HTTP request.
+    - id (int): The unique identifier of the user.
+
+    Returns:
+    - Response: A JSON response containing the user's email and username on GET, 
+                updated username on PATCH, or an empty response on DELETE.
+    """
+    
     user = get_object_or_404(CustomUser, id=id)
 
     if request.method == 'GET':
@@ -69,6 +103,16 @@ def user_details(request, id):
     
 @api_view(['GET'])
 def user(request):
+
+    """
+    Retrieves a list of all users with their details.
+
+    Args:
+    - request (Request): The incoming HTTP request.
+
+    Returns:
+    - Response: A JSON response containing a list of users with their id, last login, email, username, and creation date.
+    """
 
     if request.method == 'GET':
         data = CustomUser.objects.values('id', 'last_login', 'email', 'username', 'createdAt')
