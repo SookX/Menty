ea697fd50e70a780058b3b2248dc929473067f80
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-06-24 00:18:46 +0300

    Context Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,22 +5,24 @@ import Header from './components/Header/Header'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+import DataProvider from './context/DataContext'
 
 function App() {
   return (
     <>
+      <DataProvider>
+        <ThemeProvider theme={theme}>
+          <Header />
 
-      <ThemeProvider theme={theme}>
-        <Header />
-
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Routes>
-        </BrowserRouter> 
-      </ThemeProvider>    
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+            </Routes>
+          </BrowserRouter> 
+        </ThemeProvider>    
+      </DataProvider>
     </>
   )
 }
---
+++ client/src/context/DataContext.jsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+
+export const DataContext = createContext({ })
+
+const DataProvider = ({ children }) => {
+    
+
+    return (
+        <DataContext.Provider
+            value={{
+
+            }}
+        >
+            {children}
+        </DataContext.Provider>
+    )
+}
+
+export default DataProvider
