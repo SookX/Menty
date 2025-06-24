77296efc32c7f72c9a1ef1064b4e1825a42820a4
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-06-24 15:05:05 +0300

    Set up data card

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -3,12 +3,28 @@ import Section from "../../components/Section/Section"
 import { useContext } from "react"
 import { DataContext } from "../../context/DataContext"
 import { useState } from "react"
-import { Box, Grid, Typography } from "@mui/material"
+import { Box, Card, Grid, styled, Typography } from "@mui/material"
 import DataCard from "./components/DataCard/DataCard"
 import PromptCard from "./components/PromptCard/PromptCard"
 import AdviceCard from "./components/AdviceCard/AdviceCard"
+import { createContext } from "react"
+
+export const DashboardContext = createContext({  })
+
+const StyledGrid = styled(Grid)(({theme})=>({
+    padding: "48px 64px",
+    paddingTop: "128px"
+}))
 
 const Dashboard = () => {
+    // Holds the styles for each section
+    const StyledCard = styled(Card)(({theme})=>({
+        borderRadius: theme.shape.sectionBorderRadius,
+        padding: "40px"
+    }))
+
+
+
     // Gets global data from the context
     const { crud } = useContext(DataContext)
 
@@ -41,19 +57,27 @@ const Dashboard = () => {
 
 
     return (
-        <Section>
-            <Grid container rowGap={4} columnGap={6}>
-                <Grid size={8}>
-                    <DataCard />
-                </Grid>
-                <Grid size={4}>
-                    <PromptCard />
-                </Grid>
-                <Grid size={"grow"}>
-                    <AdviceCard />
-                </Grid>
-            </Grid>
-        </Section>
+        <DashboardContext.Provider value={{
+            StyledCard,
+            user
+        }}>
+            <Section>
+                <StyledGrid container rowGap={4} columnGap={6}>
+                    {
+                        user &&
+                        <Grid size={8}>
+                            <DataCard />
+                        </Grid>
+                    }
+                    <Grid size={4}>
+                        <PromptCard />
+                    </Grid>
+                    <Grid size={"grow"}>
+                        <AdviceCard />
+                    </Grid>
+                </StyledGrid>
+            </Section>
+        </DashboardContext.Provider>
     )
 }
 
--- client/src/pages/Dashboard/components/DataCard/DataCard.jsx
+++ client/src/pages/Dashboard/components/DataCard/DataCard.jsx
@@ -1,6 +1,22 @@
+import { useContext } from "react"
+import { DashboardContext } from "../../Dashboard"
+import { Box, Typography } from "@mui/material"
+
 const DataCard = () => {
+    // Gets dashboard data from the context
+    const { StyledCard, user } = useContext(DashboardContext)
+
+
+
     return (
-        <>DS</>
+        <StyledCard sx={{ backgroundColor: "primary.tint8", color: "text.tint3" }}>
+            { user && user.username && <Typography variant="h3" color="primary">{user.username}</Typography> }
+            { user && user.email && <Typography mb={1.5} variant="body1" textTransform="uppercase" fontWeight="bold" color="secondary.tint3">{user.email}</Typography> }
+
+            <Typography variant="body1">Welcome to your dashboard! Here you can find your mental health progress as well as your daily tips for improvement. The chart below shows your mental state over the last 30 days.</Typography>
+
+            <Box>Chart :{`(`}</Box>
+        </StyledCard>
     )
 }
 
